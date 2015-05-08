9f2d75add73bca86ba2b9022987fa957b4b21897
jylee <wjddus6@nate.com>
2015-05-08 15:36:30 +0900

    xxx

---
+++ src/main/java/com/webapp/model/LoadSearch.java
@@ -0,0 +1,19 @@
+package com.webapp.model;
+
+public class LoadSearch {
+	private String city;
+	private String category;
+	
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+}

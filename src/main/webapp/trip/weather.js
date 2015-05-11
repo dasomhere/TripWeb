97161a4053ee02b366200486ef11d3703ed70144
dsnam <ska465@naver.com>
2015-05-11 12:25:38 +0900

    asdfasdfsd

--- src/main/webapp/trip/weather.js
+++ src/main/webapp/trip/weather.js
@@ -9,31 +9,30 @@ myApp.controller('weatherController', function($scope, $http) {
 	
 	$scope.weather = function(woeid, city) {
 		$http.post("/TripWeb/m/weather/weather", {woeid : woeid, cityName : city}).success(function(weather) {
-//			$http.get("weatherLanguageChange.json").success(function(response) {
-//				
-//				$scope.days = response.days;
-//				$scope.conditions = response.conditions;
-//				
-//				for(var i=0; i<weather.day.length; i++) {
-//					for(var j=0; j<$scope.days.length; j++) {
-//						if(weather.day[i] == $scope.days[j].day)
-//							weather.day[i] = $scope.days[j].dayKR;
-//					}
-//				}
-//				
-//				for(var i=0; i<weather.code.length; i++) {
-//					weather.text[i] = $scope.conditions[weather.code[i]].text;
-//				}
-//				
-//				weather.nowConditionText = $scope.conditions[weather.nowConditionCode].text;
-//				
-////				alert(JSON.stringify(weather));
-//				$scope.$parent.weather = weather;
-//				location.href="#weatherResult";
-//			}).error(function() {
-//				alert("weather error...");
-//			});
-//		});
+			$http.get("weatherLanguageChange.json").success(function(response) {
+				
+				$scope.days = response.days;
+				$scope.conditions = response.conditions;
+				
+				for(var i=0; i<weather.day.length; i++) {
+					for(var j=0; j<$scope.days.length; j++) {
+						if(weather.day[i] == $scope.days[j].day)
+							weather.day[i] = $scope.days[j].dayKR;
+					}
+				}
+				
+				for(var i=0; i<weather.code.length; i++) {
+					weather.text[i] = $scope.conditions[weather.code[i]].text;
+				}
+				
+				weather.nowConditionText = $scope.conditions[weather.nowConditionCode].text;
+				
+//				alert(JSON.stringify(weather));
+				$scope.$parent.weather = weather;
+				location.href="#weatherResult";
+			}).error(function() {
+				alert("weather error...");
+			});
 		});
 	};
 	

5e4732b63e11c5ccba5e80ade3b9d9dcb0672627
dsnam <ska465@naver.com>
2015-05-14 09:48:46 +0900

    zzzzzz

---
+++ src/main/java/com/webapp/model/request/DetailIntroRequestParameter.java
@@ -0,0 +1,40 @@
+package com.webapp.model.request;
+
+import java.net.URLEncoder;
+
+
+public class DetailIntroRequestParameter extends RequestParameter {
+	String serviceUrl = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/detailIntro";
+	String contentId = "";
+	String contentTypeId = "";
+	
+	public String getQueryString() {
+		String queryString="";
+		queryString += super.getQueryString();
+		queryString += "&contentId=" + contentId;
+		queryString += "&contentTypeId=" + contentTypeId;
+		
+		return queryString;
+	}
+	public String getUrl() {
+		
+		return serviceUrl + "?" + getQueryString();
+	}
+	
+	public String getContentId() {
+		return contentId;
+	}
+	public void setContentId(String contentId) {
+		this.contentId = contentId;
+	}
+	public String getContentTypeId() {
+		return contentTypeId;
+	}
+	public void setContentTypeId(String contentTypeId) {
+		this.contentTypeId = contentTypeId;
+	}
+	
+	
+	
+
+}

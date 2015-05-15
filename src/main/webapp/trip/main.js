1bf78181bbf8ab10c1d8da2370ca0eb9a145557c
jylee <wjddus6@nate.com>
2015-05-15 10:44:05 +0900

    ui-bootstrap setting

--- src/main/webapp/trip/main.js
+++ src/main/webapp/trip/main.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('myApp', ["ngSanitize", "ngAnimate", "ngRoute"]);
+var myApp = angular.module('myApp', ["ngSanitize", "ngAnimate", "ngRoute", "ui.bootstrap"]);
 
 myApp.config(function($routeProvider) {
 

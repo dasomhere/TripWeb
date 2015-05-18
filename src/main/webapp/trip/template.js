0dc7f1616ebd596e382d74215cb29dd6df8a1d92
teacher <protocls@nate.com>
2015-05-18 10:44:22 +0900

    template 추가

---
+++ src/main/webapp/trip/template.js
@@ -0,0 +1,38 @@
+
+myApp.controller('templateController', function($scope, $http) {
+	$scope.$parent.pageClass = 'page-load';
+	
+	$scope.currentPage = 1;
+	$scope.itemsPerPage = 10;
+	
+	$scope.changePage = function() {
+		console.log("page = " + $scope.currentPage);
+		
+		var keyword=$("#keyword").val();
+		if(keyword == '') {
+			return false;
+		}
+		
+		$http.get("/TripWeb/m/search/search?keyword=" + keyword + "&numOfRows=" + $scope.itemsPerPage + "&pageNo=" + $scope.currentPage).success(function(searchResult) {
+			$scope.searchResult = searchResult.response.body;
+		}).error(function() {
+			alert("search error...");
+		});
+		
+	};
+	
+	$scope.search = function() {
+		var keyword=$("#keyword").val();
+		if(keyword == '') {
+			return false;
+		}
+		$http.get("/TripWeb/m/search/search?keyword=" + keyword + "&numOfRows=" + $scope.itemsPerPage + "&pageNo=" + $scope.currentPage).success(function(searchResult) {
+			$scope.searchResult = searchResult.response.body;
+		}).error(function() {
+			alert("search error...");
+		});
+		
+	};
+	
+	
+});

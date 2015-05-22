ebfa4cf1a183eac52f34130202518922597a4b17
jylee <wjddus6@nate.com>
2015-05-22 14:06:43 +0900

    xxx

--- src/main/java/com/webapp/controller/LoadController.java
+++ src/main/java/com/webapp/controller/LoadController.java
@@ -1,46 +1,17 @@
 package com.webapp.controller;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-
-import javax.xml.soap.Detail;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonSyntaxException;
-import com.webapp.model.City;
-import com.webapp.model.DetailInfo;
-import com.webapp.model.GuanGuangJi;
-import com.webapp.model.Gus;
-import com.webapp.model.LoadCategory;
-import com.webapp.model.LoadDetailInfo;
-import com.webapp.model.LoadSearch;
-import com.webapp.model.Weather;
 import com.webapp.model.request.AreaBasedListRequestParameter;
 import com.webapp.model.request.AreaCodeRequestParameter;
 import com.webapp.model.request.CategoryCodeRequestParameter;
 import com.webapp.model.request.DetailInfoRequestParameter;
-import com.webapp.model.response.Items;
 import com.webapp.model.response.ResponseMessage;
 import com.webapp.service.TourApiService;
 
--- src/main/java/com/webapp/controller/MapController.java
+++ src/main/java/com/webapp/controller/MapController.java
@@ -1,41 +1,15 @@
 package com.webapp.controller;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.webapp.model.GuanGuangJi;
-import com.webapp.model.Gus;
-import com.webapp.model.LocalCategory;
-import com.webapp.model.LocalDetailImage;
-import com.webapp.model.LocalDetailInfo;
-import com.webapp.model.LocalSearch;
 import com.webapp.model.request.AreaBasedListRequestParameter;
 import com.webapp.model.request.AreaCodeRequestParameter;
-import com.webapp.model.request.CategoryCodeRequestParameter;
-import com.webapp.model.request.DetailCommonRequestParameter;
-import com.webapp.model.request.DetailInfoRequestParameter;
-import com.webapp.model.request.DetailIntroRequestParameter;
-import com.webapp.model.request.StayInfoRequestParameter;
-import com.webapp.model.request.ImageInfoRequestParameter;
-import com.webapp.model.request.SearchStayRequestParameter;
 import com.webapp.model.response.ResponseMessage;
 import com.webapp.service.TourApiService;
 

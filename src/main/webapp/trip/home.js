5942d4af9727981347ef69b947fbba46d4ca7937
yjpark <pyj8470@naver.com>
2015-05-28 09:51:48 +0900

    home

--- src/main/webapp/trip/home.js
+++ src/main/webapp/trip/home.js
@@ -6,71 +6,71 @@ var myApp = angular.module('homeApp', ["ngSanitize", "ngAnimate", "ngRoute", "ng
 
 myApp.config(function($routeProvider) {
 
-//	var event = {
-//			templateUrl : "event.jsp",
-//			controller : "eventController"
-//	};
-//	var local = {
-//			templateUrl : "local.jsp",
-//			controller : "localController"
-//	};
-//	var map = {
-//			templateUrl : "map.jsp",
-//			controller : "mapController"
-//	};
-//	var maplocation = {
-//			templateUrl : "maplocation.jsp",
-//			controller : "maplocationController"
-//	};
-//	var searchDetail = {
-//			templateUrl : "searchDetail.jsp",
-//			controller : "searchDetailController"
-//	};
-//	var weather = {
-//			templateUrl : "weather.jsp",
-//			controller : "weatherController"
-//	};
-//	var weatherResult = {
-//			templateUrl : "weatherResult.jsp",
-//			controller : "weatherResultController"
-//	};
-//	var load = {
-//			templateUrl : "load.jsp",
-//			controller : "loadController"
-//	};
-//	var login = {
-//			templateUrl : "login.jsp",
-//			controller : "loginController"
-//	};
-//	var join = {
-//			templateUrl : "join.jsp",
-//			controller : "joinController"
-//	};
-//	
-//	var template = {
-//			templateUrl : "template.jsp",
-//			controller : "templateController"
-//	};
-//	
-//	var home = {
-//			templateUrl : "home.jsp"
-//	};
+	var event = {
+			templateUrl : "event.jsp",
+			controller : "eventController"
+	};
+	var local = {
+			templateUrl : "local.jsp",
+			controller : "localController"
+	};
+	var map = {
+			templateUrl : "map.jsp",
+			controller : "mapController"
+	};
+	var maplocation = {
+			templateUrl : "maplocation.jsp",
+			controller : "maplocationController"
+	};
+	var searchDetail = {
+			templateUrl : "searchDetail.jsp",
+			controller : "searchDetailController"
+	};
+	var weather = {
+			templateUrl : "weather.jsp",
+			controller : "weatherController"
+	};
+	var weatherResult = {
+			templateUrl : "weatherResult.jsp",
+			controller : "weatherResultController"
+	};
+	var load = {
+			templateUrl : "load.jsp",
+			controller : "loadController"
+	};
+	var login = {
+			templateUrl : "login.jsp",
+			controller : "loginController"
+	};
+	var join = {
+			templateUrl : "join.jsp",
+			controller : "joinController"
+	};
+	
+	var template = {
+			templateUrl : "template.jsp",
+			controller : "templateController"
+	};
+	
+	var home = {
+			templateUrl : "home.jsp"
+	};
 	
 	
-//	$routeProvider.when('/event', event);
-//	$routeProvider.when('/local', local);
-//	$routeProvider.when('/map', map);
-//	$routeProvider.when('/maplocation', maplocation);
-//	$routeProvider.when('/search', search);
-//	$routeProvider.when('/searchDetail/:contentid/:contenttypeid', searchDetail);
-//	$routeProvider.when('/weather', weather);
-//	$routeProvider.when('/weatherResult/:woeid/:city', weatherResult);
-//	$routeProvider.when('/load', load);
-//	$routeProvider.when('/login', login);
-//	$routeProvider.when('/join', join);
-//	$routeProvider.when('/template', template);
-//	$routeProvider.when('/home', home);
-//	$routeProvider.otherwise({redirectTo:'/'});
+	$routeProvider.when('/event', event);
+	$routeProvider.when('/local', local);
+	$routeProvider.when('/map', map);
+	$routeProvider.when('/maplocation', maplocation);
+	$routeProvider.when('/search', search);
+	$routeProvider.when('/searchDetail/:contentid/:contenttypeid', searchDetail);
+	$routeProvider.when('/weather', weather);
+	$routeProvider.when('/weatherResult/:woeid/:city', weatherResult);
+	$routeProvider.when('/load', load);
+	$routeProvider.when('/login', login);
+	$routeProvider.when('/join', join);
+	$routeProvider.when('/template', template);
+	$routeProvider.when('/home', home);
+	$routeProvider.otherwise({redirectTo:'/'});
 	
 });
 
